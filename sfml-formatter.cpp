73738acbdf5c905cb6d635d2d9ee35d0cd2e2cb4
Кирилл Скрипников <Kirill.Skripnikow@yandex.ru>
2025-07-15 15:37:42 +0300

    Добавлены заголовки

--- sfml-formatter.cpp
+++ sfml-formatter.cpp
@@ -3,31 +3,46 @@
 
 #include <iostream>
 #include <SFML/Graphics.hpp>
-
+using namespace std;
 int main()
 {
+    
 
     sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
-    sf::CircleShape circle(20.f);
-    circle.setFillColor(sf::Color::Cyan);
+  
+    sf::Font font;
+    sf::Text header;
+    sf::Text subtitle;
+    font.loadFromFile("./font/arialbd.ttf");
+    
+
+    header.setFont(font);
+    header.setString("values formatter");
+    header.setCharacterSize(24);
+
+    subtitle.setFont(font);
+    subtitle.setString("input your value: ...");
+    subtitle.setCharacterSize(18);
+    subtitle.setPosition(sf::Vector2f(0,30));
 
     while (window.isOpen())
     {
-  
+
         sf::Event event;
         while (window.pollEvent(event))
         {
-    
+
             if (event.type == sf::Event::Closed)
                 window.close();
         }
 
 
         window.clear();
-        window.draw(circle);
-     
-        window.display();
-    }
 
-    return EXIT_SUCCESS;
+        window.draw(header);
+        window.draw(subtitle);
+
+        window.display();
+
+    }
 }
