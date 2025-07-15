365647b549cd01aad8479f9bbe193ce34227994a
Кирилл Скрипников <Kirill.Skripnikow@yandex.ru>
2025-07-15 14:09:09 +0300

    Создан базовый проект SFML

---
+++ sfml-formatter.cpp
@@ -0,0 +1,33 @@
+﻿// sfml-formatter.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+    sf::CircleShape circle(20.f);
+    circle.setFillColor(sf::Color::Cyan);
+
+    while (window.isOpen())
+    {
+  
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+    
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+
+        window.clear();
+        window.draw(circle);
+     
+        window.display();
+    }
+
+    return EXIT_SUCCESS;
+}
