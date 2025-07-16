eafb218ada4aa992a7cc5a0db2967199ef6f142c
Кирилл Скрипников <Kirill.Skripnikow@yandex.ru>
2025-07-16 19:48:11 +0300

    home

--- sfml-formatter.cpp
+++ sfml-formatter.cpp
@@ -3,18 +3,45 @@
 
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include <vector>
 using namespace std;
 int main()
 {
     
 
     sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
-  
+    sf::RectangleShape btn(sf::Vector2f(50.f, 30.f));
+    
     sf::Font font;
     sf::Text header;
     sf::Text subtitle;
+   
     font.loadFromFile("./font/arialbd.ttf");
-    
+
+    vector <sf::RectangleShape> btns;
+    vector <sf::Text> btnsText;
+
+    for (int i = 0; i < 10; i++)
+    {
+        sf::RectangleShape btn(sf::Vector2f(50.f, 30.f));
+        sf::Text text;
+
+        text.setFont(font);
+        text.setCharacterSize(18);
+        text.setString(to_string(i));
+        text.setFillColor(sf::Color::Black);
+
+        
+        btn.setPosition(sf::Vector2f(i*60, 100));
+        btns.push_back(btn);
+        sf::Vector2f position = btn.getPosition();
+
+        text.setPosition(sf::Vector2f(position.x+10, 110));
+        btnsText.push_back(text);
+
+       
+    }
+   
 
     header.setFont(font);
     header.setString("values formatter");
@@ -41,7 +68,12 @@ int main()
 
         window.draw(header);
         window.draw(subtitle);
-
+        for (auto i : btns) {
+            window.draw(i);
+        }
+        for (auto i : btnsText) {
+            window.draw(i);
+        }
         window.display();
 
     }
